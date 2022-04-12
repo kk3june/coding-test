722112e8b866fdb58b79e866f0210e814536cb3c
3jun <34735492+kk3june@users.noreply.github.com>
2022-04-12 09:23:52 +0900

    [Bronze IV] Title: 윤년, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+
+const num = Number(input);
+if ( num % 4 === 0 && num % 100 != 0 || num % 400 === 0 ) {
+  console.log(1);
+} else {
+  console.log(0);
+}
