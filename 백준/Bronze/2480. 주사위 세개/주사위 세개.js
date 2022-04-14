e2b663d5ec386f3b94c2ba8a6fa201d407469bf5
3jun <34735492+kk3june@users.noreply.github.com>
2022-04-15 00:35:19 +0900

    [Bronze IV] Title: 주사위 세개, Time: 148 ms, Memory: 9440 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -3,25 +3,25 @@ const readline = require("readline")
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-})
+});
 
-let input = []
+let input = [];
 
 rl.on("line", function(line) {
-    input = line.split(' ').map((el) => parseInt(el))
+    input = line.split(' ').map((el) => parseInt(el));
 }).on("close", function() {
-    let dice1 = input[0]
-    let dice2 = input[1]
-    let dice3 = input[2]
-    let maxDice = Math.max(...input)
+    let dice1 = input[0];
+    let dice2 = input[1];
+    let dice3 = input[2];
+    let maxDice = Math.max(...input);
     if(dice1 === dice2 && dice1 === dice3) {
-        console.log(10000 + dice1 * 1000)
+        console.log(10000 + dice1 * 1000);
     } else if(dice1 === dice2 || dice1 === dice3) {
-        console.log(1000 + dice1 * 100)
+        console.log(1000 + dice1 * 100);
     } else if(dice2 === dice3) {
-        console.log(1000 + dice2 * 100)
+        console.log(1000 + dice2 * 100);
     } else {
-        console.log(maxDice * 100)
+        console.log(maxDice * 100);
     }
     process.exit();
-})
+});
