657d61e16ed28f27d45df95c3b0673b0689f07ab
3jun <34735492+kk3june@users.noreply.github.com>
2022-04-20 22:13:19 +0900

    [Bronze III] Title: A+B - 7, Time: 148 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+
+
+rl.on('line', function (line) {
+    input.push(line);
+}).on('close', function () {
+
+    const T = Number(input[0]);
+
+    for (let i = 1; i <= T; i++) {
+        let num1 = +input[i].split(' ')[0];
+        let num2 = +input[i].split(' ')[1];
+
+        console.log(`Case #${i}: ${num1 + num2}`);
+    }
+
+    process.exit();
+});
