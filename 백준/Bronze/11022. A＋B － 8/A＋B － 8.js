2c012c7a843fd33effbcd296a0df51bb0db2511f
3jun <34735492+kk3june@users.noreply.github.com>
2022-04-21 21:52:49 +0900

    [Bronze III] Title: A+B - 8, Time: 184 ms, Memory: 9664 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', function(line){
+    input.push(line);
+    
+}).on('close', function(){
+    for(let i = 1; i <= parseInt(input[0]); i++) {
+        let num1 = parseInt(input[i].split(' ')[0]);
+        let num2 = parseInt(input[i].split(' ')[1]);
+        console.log(`Case #${i}: ${num1} + ${num2} = ${num1 + num2}`)
+    }
+    
+    process.exit();
+})
