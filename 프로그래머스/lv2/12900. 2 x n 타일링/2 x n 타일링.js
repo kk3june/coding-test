1735c9ceb51be25d545980e0204edad3b264c4f3
3jun <34735492+kk3june@users.noreply.github.com>
2023-04-29 20:23:26 +0900

    [level 2] Title: 2 x n 타일링, Time: 3.20 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,11 +1,9 @@
 function solution(n) {
-  let answer = Array(n).fill(0);
-  answer[1] = 1;
-  answer[2] = 2;
-  for (let i = 3; i <= n; i++) {
-    let current = answer[i - 2] + answer[i - 1];
-    answer[i] = current % 1000000007;
-  }
-
-  return answer[n];
+    const dp = Array(n-1).fill(0)
+    dp[0] = 1
+    dp[1] = 2
+    for(var i = 2; i < n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
+    }
+    return dp[n-1];
 }
