18a5f7c5eb5ddae2c1554d87993f252b987c1de4
3jun <34735492+kk3june@users.noreply.github.com>
2025-07-24 14:12:01 +0900

    [Gold III] Title: 아침 산책, Time: 1136 ms, Memory: 137652 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.js
+++ 백준/Gold/21606. 아침 산책/아침 산책.js
@@ -1,46 +1,44 @@
 const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
+ .readFileSync("/dev/stdin")
+ .toString()
+ .trim()
+ .split("\n");
 
-const n = +input[0];
-const type = input[1].split('').map(Number);
-const graph = Array.from({length: n + 1}, () => []);
+const N = +input[0];
+const inside = '0' + input[1];
+const graph = Array.from({length: N + 1}, () => []);
+const visited = Array(N + 1).fill(false);
+let total = 0;
 
-for(let i = 2; i < input.length; i++) {
-  const [a, b] = input[i].split(' ').map(Number);
-  graph[a].push(b);
-  graph[b].push(a);
+for(let i = 0; i < N - 1; i++) {
+   const [a, b] = input[i + 2].split(' ').map(Number);
+   graph[a].push(b);
+   graph[b].push(a);
+   
+   if(inside[a] === "1" && inside[b] === "1") {
+       total += 2;
+   }
 }
 
-function getRoutes() {
-  let count = 0;
-  for(let i = 1; i <= n; i++) {
-    if(type[i] === 1) {
-      const visited = Array(n+1).fill(false);
-      visited[i] = true;
-      const queue = [i];
-
-      while(queue.length > 0) {
-        const current = queue.shift();
-
-        for(let neighbor of graph[current]) {
-          if(!visited[neighbor]) {
-            if(type[neighbor - 1] === 0) {
-              visited[neighbor] = true;
-              queue.push(neighbor);
-            } else {
-              if(neighbor !== i) {
-                count++;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  console.log(count);
+function DFS(start) {
+   visited[start] = true;
+   let inside_count = 0;
+   
+   for(let v of graph[start]) {
+       if(inside[v] === '1') {
+           inside_count++;
+       } else if(!visited[v] && inside[v] === "0") {
+           inside_count += DFS(v);
+       }
+   }
+   return inside_count;
 }
 
-getRoutes();
+for(let i = 1; i <= N; i++) {
+   if(inside[i] === '0' && !visited[i]) {
+       const result = DFS(i);
+       total += result * (result - 1);
+   }
+}
+
+console.log(total);
