608b7680cb2d58d92b923859a4a0b3c2765b7908
3jun <34735492+kk3june@users.noreply.github.com>
2025-08-14 15:57:14 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 152 ms, Memory: 9860 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -1,35 +1,34 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const N = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
 
-const input = [];
-rl.on('line', (line) => {
-    input.push(line.trim());
-}).on('close', () => {
-    const N = Number(input[0]);
-    const arr = input[1].split(' ').map(Number);
-    const lis = [];
-
+function solution() {
+    const lis = []; 
     function lowerBound(arr, target) {
-        let left = 0, right = arr.length;
+        let left = 0;
+        let right = arr.length;
+        
         while (left < right) {
-            let mid = Math.floor((left + right) / 2);
-            if (arr[mid] >= target) right = mid;
-            else left = mid + 1;
+            const mid = Math.floor((left + right) / 2);
+            if (arr[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
         return left;
     }
-
-    for (let num of arr) {
-        let idx = lowerBound(lis, num);
+    
+    for (const num of arr) {
+        const idx = lowerBound(lis, num); 
         if (idx === lis.length) {
             lis.push(num);
         } else {
             lis[idx] = num;
         }
     }
-
+    
     console.log(lis.length);
-});
+}
+
+solution();
